d7bdfb66083c6680bb5dc28316abc701439cfe92
Mdcosta <mdcosta@latam.stefanini.com>
2023-04-27 22:56:51 -0300

    update home

--- reserve/src/app/app.module.ts
+++ reserve/src/app/app.module.ts
@@ -9,9 +9,19 @@ import { CadastroComponent } from './views/cadastro/cadastro.component';
 import { PesquisaComponent } from './views/pesquisa/pesquisa.component';
 import { ReservaComponent } from './views/reserva/reserva.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { HttpClientModule } from '@angular/common/http';
+import { OAuthModule } from 'angular-oauth2-oidc';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
+    AppComponent,
     AppComponent,
     HomeComponent,
     LoginComponent,
@@ -19,7 +29,29 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     PesquisaComponent,
     ReservaComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    OAuthModule.forRoot(),
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatCardModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- reserve/src/app/views/home/home.component.ts
+++ reserve/src/app/views/home/home.component.ts
@@ -1,15 +1,78 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  gap = 16;
 
-  constructor() { }
+  carousel = document.getElementById('carousel');
+  content = document.getElementById('content');
+  next = document.getElementById('next');
+  prev = document.getElementById('prev');
+  width = this.carousel?.offsetWidth;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  arr: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  optionsArr: number[] = [1, 2, 3, 4];
+  totalCards: number = this.arr.length;
+  currentPage: number = 1;
+  pagePosition: string = '0%';
+  cardsPerPage!: number;
+  totalPages!: number;
+  overflowWidth!: string;
+  cardWidth!: string;
+  containerWidth!: number;
+  @ViewChild('container', { static: true, read: ElementRef })
+  container!: ElementRef;
+  @HostListener('window:resize') windowResize() {
+    let newCardsPerPage = this.getCardsPerPage();
+    if (newCardsPerPage != this.cardsPerPage) {
+      this.cardsPerPage = newCardsPerPage;
+      this.initializeSlider();
+      if (this.currentPage > this.totalPages) {
+        this.currentPage = this.totalPages;
+        this.populatePagePosition();
+      }
+    }
   }
 
+  ngOnInit() {
+    this.cardsPerPage = this.getCardsPerPage();
+    this.initializeSlider();
+  }
+
+  initializeSlider() {
+    this.totalPages = Math.ceil(this.totalCards / this.cardsPerPage);
+    this.overflowWidth = `calc(${this.totalPages * 100}% + ${
+      this.totalPages * 10
+    }px)`;
+    this.cardWidth = `calc((${100 / this.totalPages}% - ${
+      this.cardsPerPage * 10
+    }px) / ${this.cardsPerPage})`;
+  }
+
+  getCardsPerPage() {
+    return Math.floor(this.container.nativeElement.offsetWidth / 200);
+  }
+
+  changePage(incrementor: any) {
+    this.currentPage += incrementor;
+    this.populatePagePosition();
+  }
+
+  populatePagePosition() {
+    this.pagePosition = `calc(${-100 * (this.currentPage - 1)}% - ${
+      10 * (this.currentPage - 1)
+    }px)`;
+  }
 }
